894267d8617bd3d9058e607aa8559c64bc25a94a
csyrooney <csyrooney@gmail.com>
2014-09-23 21:06:46 +0800

    第二次提交

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -16,7 +16,7 @@ public class DisplayMessageActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
-
+ 
 		    Intent intent = getIntent();
 		    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
